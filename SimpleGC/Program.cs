3531401e5f4fd8bead4e323af5a1a45c139b0bf7
AlexDev <minino-forever@rambler.ru>
2025-08-09 20:21:06 +0700

    modified project SimpleGC

--- SimpleGC/Program.cs
+++ SimpleGC/Program.cs
@@ -21,6 +21,35 @@
             Console.WriteLine(refToMyCar2.ToString());
 
             Console.WriteLine($"Generation of refToMyCar is: {GC.GetGeneration(refToMyCar2)}");
+
+            object[] tonsOfObjects = new object[50000];
+
+            for (int i = 0; i < 50000; i++)
+            {
+                tonsOfObjects[i] = new object();
+            }
+
+            Console.WriteLine("Force Garbage Collection");
+
+            GC.Collect(0, GCCollectionMode.Forced);
+
+            GC.WaitForPendingFinalizers();
+
+            Console.WriteLine($"Generation of refToMyCar2 is: {GC.GetGeneration(refToMyCar2)}");
+
+            if (tonsOfObjects[9000] != null){
+                Console.WriteLine($"Generation of tonsOfObjects[9000] is: {GC.GetGeneration(tonsOfObjects[9000])}");
+            }
+            else
+            {
+                Console.WriteLine($"tonsOfObject[9000] is no longer alive.");
+            }
+
+            Console.WriteLine($"\nGen 0 has been swet {GC.CollectionCount(0)} times.");
+
+            Console.WriteLine($"Gen 1 has been sweet {GC.CollectionCount(1)} times.");
+
+            Console.WriteLine($"Gen 2 has been swet {GC.CollectionCount(2)} times.");
         }
 
         static void MakeACar()
