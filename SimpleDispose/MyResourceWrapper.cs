3ffbd5ffed55169d69da212540b8498fb14b592e
AlexDev <minino-forever@rambler.ru>
2025-08-09 22:31:18 +0700

    create class MyResourse/wrapper

---
+++ SimpleDispose/MyResourceWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDispose
+{
+    internal class MyResourceWrapper : IDisposable
+    {
+        public void Dispose()
+        {
+            //Testing
+            Console.WriteLine("******In Dispose!******");
+        }
+    }
+}
---
+++ SimpleDispose/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SimpleDispose
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****Fun with Dispose****\n");
+
+            MyResourceWrapper rw = new MyResourceWrapper();
+
+            if (rw is IDisposable)
+            {
+                rw.Dispose();
+            }
+
+            Console.WriteLine("\nDemonsrate using declarations");
+
+            UsingDeclaration();
+        }
+
+        private static void UsingDeclaration()
+        {
+            using var rw = new MyResourceWrapper();
+
+            Console.WriteLine("About to dispose");
+        }
+    }
+}
