7c35ab87480c31217ea1ce83e9716cdb51ef5a5d
AlexDev <minino-forever@rambler.ru>
2025-08-09 19:47:42 +0700

    created project SimpleGC

---
+++ SimpleGC/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SimpleGC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********GC Basic*******\n");
+
+            Car refToMyCar = new Car("Zippy", 50);
+
+            Console.WriteLine($"{refToMyCar.ToString()}\n");
+
+            Console.WriteLine("*******Fun with System.GC*****\n");
+
+            Console.WriteLine($"Estimated bytes on heap: {GC.GetTotalMemory(false)}");
+
+            Console.WriteLine($"This OS has {GC.MaxGeneration + 1} object generation.\n");
+
+            Car refToMyCar2 = new Car("Zippy", 100);
+
+            Console.WriteLine(refToMyCar2.ToString());
+
+            Console.WriteLine($"Generation of refToMyCar is: {GC.GetGeneration(refToMyCar2)}");
+        }
+
+        static void MakeACar()
+        {
+            Car myCar = new Car();
+
+            myCar = null;
+        }
+    }
+}
